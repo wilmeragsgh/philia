cf9137bc10790d820bcb45612a4bcfa18e18f9e8
wilmeragsgh <wilmeragsgm@gmail.com>
2021-05-14 22:05:13 -0400

    feat: Layout initial structure for chalice setup

--- start.py
+++ start.py
@@ -3,23 +3,58 @@ import re
 
 from dotenv import find_dotenv, load_dotenv
 # Use the package we installed
+from chalice import Chalice, Response
 from slack_bolt import App
+from slack_bolt.adapter.aws_lambda.chalice_handler import ChaliceSlackRequestHandler
+
+
 
 load_dotenv(find_dotenv())
 
 # Initializes your app with your bot token and signing secret
-app = App(
+bolt_app = App(
+    process_before_response=True,
     token=os.environ.get("SLACK_BOT_TOKEN"),
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
 )
 
-@app.message(re.compile("."))
+
+@bolt_app.command("/start")
+def respond_to_slack_within_3_seconds(ack):
+    ack("one sec!")
+
+@bolt_app.message(re.compile("."))
 def say_some(message, say):
     user = message['user']
     content = message["text"]
     print(message, flush=True)
     say(f"This *{content}* message is a little harsh, <@{user}>!")
 
-# Start your app
-if __name__ == "__main__":
-    app.start(port=int(os.environ.get("PORT", 3000)))
+
+ChaliceSlackRequestHandler.clear_all_log_handlers()
+logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG)
+
+
+app = Chalice(app_name="philia-slack")
+slack_handler = ChaliceSlackRequestHandler(app=bolt_app, chalice=app)
+
+
+@app.route(
+    "/slack/events",
+    methods=["POST"],
+    content_types=["application/x-www-form-urlencoded", "application/json"],
+)
+def events() -> Response:
+    return slack_handler.handle(app.current_request)
+
+
+@app.route("/slack/install", methods=["GET"])
+def install() -> Response:
+    return slack_handler.handle(app.current_request)
+
+
+@app.route("/slack/oauth_redirect", methods=["GET"])
+def oauth_redirect() -> Response:
+    return slack_handler.handle(app.current_request)
+
+
